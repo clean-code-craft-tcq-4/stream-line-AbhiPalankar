8064f3f831d755bec98571bf29a2436dc3d55f19
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-19 09:17:06 +0530

    Delete BMS_SensorData_Receiver.h

--- BMS_SensorData_Receiver.h
+++
@@ -1,4 +0,0 @@
-void ReadBatteryParameter(int min,int max,int *ReadBatteryTemp,int *ReadBatterySoc);
-
-int Simple_Moving_Avg(int *arrNumbers, long *sum, int pos, int len, int nextNum);
-void Calculate_Avg(int *ReadBatteryTemp,int *ReadBatterySoc);
