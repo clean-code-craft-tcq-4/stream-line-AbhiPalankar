ff3d49ccd3b7ff1ded1f64ba3c7c01dc372f8f68
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-19 09:23:53 +0530

    Update BMS_SenderData_tester.c

--- BMS_SenderData_tester.c
+++ BMS_SenderData_tester.c
@@ -2,8 +2,6 @@
 #include "BMS_SensorData_Sender.h"
 #include "BMS_SensorData_Receiver.h"
 
-int sampleValue[5] = {1,2,3,4,5};
-int sampleValue1[5] = {1,2,3,4,5};
 
 void Tester(){
 	BatteryParameters BatteryParam;
@@ -25,6 +23,4 @@ void Tester(){
 int main(void) {
 	sendBatteryParameters();
 	Tester();
-	ReadBatteryParameter(0,5,sampleValue,sampleValue1);
-    Calculate_Avg(sampleValue,sampleValue1);
 }
