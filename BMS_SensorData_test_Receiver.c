66ae9d8be4260ae0f15d06e280d30f0accc378ab
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-10-31 00:41:20 +0530

    Update BMS_SensorData_test_Receiver.c

--- BMS_SensorData_test_Receiver.c
+++ BMS_SensorData_test_Receiver.c
@@ -14,6 +14,18 @@ void testCalculateMinValue()
   CalculateMinValue(data, &Min);
   assert(Min == 6);
 }
+void testCalculateMaxValue()
+{
+  float data = 3;
+  float Max = 8;
+  CalculateMaxValue(data, &Max);
+  assert(Max == 8);
+  
+  data = 7;
+  Max = 8;
+  CalculateMaxValue(data, &Max);
+  assert(Max == 8);  
+}
 
 int main()
 {
