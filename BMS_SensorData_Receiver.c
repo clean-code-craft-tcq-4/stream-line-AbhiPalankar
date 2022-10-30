a2bec16cc3e53f8c775f9ad6cdaf68407adc5739
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-10-31 00:24:47 +0530

    Update BMS_SensorData_Receiver.c

--- BMS_SensorData_Receiver.c
+++ BMS_SensorData_Receiver.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include "BMS_SensorData_Receiver.h"
 
+
+
+void  CalculateMinValue(float data, float* Min)
+{
+   if (data < *Min)
+   {
+     *Min = data;
+   }
+   else
+   {
+     /*Do nothing*/
+   }
+}
+
+
+void  CalculateMaxValue(float data, float* Max)
+{
+   if (data > *Max)
+   {
+     *Max = data;
+   }
+   else
+   {
+     /*Do nothing*/
+   }
+}
