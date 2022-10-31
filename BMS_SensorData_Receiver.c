16505346157b24774dee835066706171bb7ef84b
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-10-31 10:04:07 +0530

    Update BMS_SensorData_Receiver.c

--- BMS_SensorData_Receiver.c
+++ BMS_SensorData_Receiver.c
@@ -43,3 +43,46 @@ void CalculateSMA ()
   SMASOC        = SumOfSOC/NO_OF_SAMPLES;
   SMAChargeRate = SumOfChargeRate/NO_OF_SAMPLES;
 }
+
+void  ReadBMSParametersfromConsole()
+{
+  float Temp, StateOfCharge, ChargingRate;
+  char Buffer[30];
+  fgets(Buffer,30,stdin);
+  for (int i=0; i<50; i++)
+  {
+    ReadFromConsole(&Temp, &StateOfCharge, &ChargingRate);
+    FindMinAndMax(Temp, &Min_Temp, &Max_Temp);
+    FindMinAndMax(StateOfCharge, &Min_StateOfCharge, &Max_StateOfCharge);
+    FindMinAndMax(ChargingRate, &Min_ChargingRate, &Max_ChargingRate);
+
+    CalculateSMA();
+    PrintMinMaxSMA();
+  }
+}
+
+void ReadFromConsole(float* Temp, float* StateOfCharge, float* ChargingRate)
+{
+  ReadParameters(Temp, StateOfCharge, ChargingRate);
+  
+  for(int i = 0; i<NO_OF_SAMPLES-1; i++)
+  {
+    Temperature[i] = Temperature[i+1];
+    SOC[i] = SOC[i+1];
+    ChargeRate[i] = ChargeRate[i+1];
+  }
+  
+  /*Inserting the latest data into the last element of array*/
+  Temperature[NO_OF_SAMPLES-1] = *Temp;
+  SOC[NO_OF_SAMPLES-1] = *StateOfCharge;
+  ChargeRate[NO_OF_SAMPLES-1] = *ChargingRate;
+    
+  return;
+}
+
+int ReadParameters(float* Temp, float* StateOfCharge, float* ChargingRate)
+{
+  char dummy;
+  
+  return scanf("%f%c%f%c%f", Temp, &dummy, StateOfCharge, &dummy, ChargingRate);
+}
