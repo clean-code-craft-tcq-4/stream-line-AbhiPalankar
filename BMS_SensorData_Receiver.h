ee7f48326cf2c4af9e8239ccab6cce1483236098
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-10-31 10:16:00 +0530

    Update BMS_SensorData_Receiver.h

--- BMS_SensorData_Receiver.h
+++ BMS_SensorData_Receiver.h
@@ -1,4 +1,10 @@
 #define NO_OF_SAMPLES 5
+extern float Min_Temp;
+extern float Max_Temp;
+extern float Min_StateOfCharge;
+extern float Max_StateOfCharge;
+extern float Min_ChargingRate;
+extern float Max_ChargingRate;
 extern float SMATemperature;
 extern float Temperature[NO_OF_SAMPLES];
 extern float SMASOC;
@@ -9,3 +15,5 @@ extern float ChargeRate[NO_OF_SAMPLES];
 void CalculateMinValue(float data, float* Min);
 void CalculateMaxValue(float data, float* Max);
 void CalculateSMA();
+int ReadParameters(float* temperature, float* StateOfCharge, float* ChargingRate);
+void ReadFromConsole(float* temperature, float* StateOfCharge, float* ChargingRate);
